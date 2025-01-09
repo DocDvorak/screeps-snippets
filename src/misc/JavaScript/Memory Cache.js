0040c83d814acf71229ace4f77d20ff02e8d1769
Dr. Dvorak <docdvorak@gmail.com>
2025-01-09 02:16:02 -0800

    Update Memory Cache.js
    Remove outdated comments regarding CPU costs for Memory serialization

--- src/misc/JavaScript/Memory Cache.js
+++ src/misc/JavaScript/Memory Cache.js
@@ -20,11 +20,9 @@ function wrapLoop(fn) {
     // there are two ways of saving Memory with different advantages and disadvantages
     // 1. RawMemory.set(JSON.stringify(Memory));
     // + ability to use custom serialization method
-    // - you have to pay for serialization
     // - unable to edit Memory via Memory watcher or console
     // 2. RawMemory._parsed = Memory;
     // - undocumented functionality, could get removed at any time
-    // + the server will take care of serialization, it doesn't cost any CPU on your site
     // + maintain full functionality including Memory watcher and console
 
     // this implementation uses the official way of saving Memory
